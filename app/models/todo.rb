36525b10bc1e367df98a78484ede3cd76e45ae99
Alexander Pavlyut <alex@pavlyut.com>
2017-08-22 10:22:48 +0300

    Revert "Got"
    This reverts commit b1e88485c4f3578f1756d9e7eb927706a1f69d58.

--- app/models/todo.rb
+++
@@ -1,9 +0,0 @@
-class Todo < ApplicationRecord
-  validates :text, uniqueness: true, presence: true
-  scope :completed, -> {where completed: true}
-  scope :active, -> {where completed: false}
-
-  before_create do
-    self.text = text.mb_chars.capitalize
-  end
-end
--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  resources :todos do
-    collection do
-      get :active, :completed, :clear
-    end
-  end
-  root to: 'todos#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
