68d8b66ad9de1619735d71cbb55bcf2a98141d29
Alexander Pavlyut <alex@pavlyut.com>
2017-09-07 19:06:36 +0300

    hotfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,5 +96,6 @@ ActionMailer::Base.smtp_settings = {
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
   :domain         => ENV['APP_HOST'],
+  :host           => ENV['APP_HOST'],
   :authentication => :plain,
 }
