bc80def71d41f5f3492169f2b1bb8251f13aea3c
Alexander Pavlyut <alex@pavlyut.com>
2017-09-07 19:19:19 +0300

    hotfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: ENV['APP_HOST']}
 end
 
 ActionMailer::Base.smtp_settings = {
